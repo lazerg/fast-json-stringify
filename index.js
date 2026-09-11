84e2bead5c7547da09e47ee9ad9497094ec337c7
lazerg <lazerg2@gmail.com>
2026-09-11 18:24:25 +0500

    fix: rethrow non-merge errors when merging $ref siblings

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 /* eslint no-prototype-builtins: 0 */
 
 const { RefResolver } = require('json-schema-ref-resolver')
+const { MergeError } = require('@fastify/merge-json-schemas')
 
 const Serializer = require('./lib/serializer')
 const Validator = require('./lib/validator')
@@ -119,7 +120,8 @@ function resolveRef (context, location) {
     mergedSchemaId = `__fjs_merged_${schemaIdCounter++}`
     try {
       mergeLocations(context, mergedSchemaId, [newLocation, siblingLocation])
-    } catch {
+    } catch (err) {
+      if (!(err instanceof MergeError)) throw err
       return newLocation
     }
     context.mergedRefsIds.set(mergedSchemaKey, mergedSchemaId)
